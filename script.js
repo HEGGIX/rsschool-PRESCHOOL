6d5db0d0921fcfc90e2d7d25a1415852c66f1866
Gleb Ermakovich <glebermakovich@gmail.com>
2023-01-22 02:26:00 +0300

    feat: stylized welcome section

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log( "Моя оценка " + "= " + 70 + " баллов")
+(function (){
+    const burgerItem = document.querySelector(".burger");
+    const menu = document.querySelector(".nav");
+    const menuCloseItem = document.querySelector(".nav_close");
+    burgerItem.addEventListener("click", () =>{
+        menu.classList.add ("nav_active");
+    });
+    menuCloseItem.addEventListener("click", () =>{
+        menu.classList.remove ("nav_active");
+    });
+}());
